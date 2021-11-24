2a8b1ef7035e463540b01b299cec574e626ece4e
Bikram Khanal <bikram.khanal@aiesec.net>
2021-11-24 10:45:46 -0600

    added new

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+    // for functions
+   }
 
   ngOnInit(): void {
   }
